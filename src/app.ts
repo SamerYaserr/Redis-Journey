096a1411fd455408fffba57f74e8c05fd92f3b24
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-20 18:24:05 +0300

    implement getPhotos and getPhoto controllers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
 app.use(cors());
+
+const getPhotos: RequestHandler = async (req, res, next) => {
+  const albumId = req.query.albumId;
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/photos",
+    { params: { albumId } }
+  );
+
+  res.json(data);
+};
+
+const getPhoto: RequestHandler = async (req, res, next) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+  );
+
+  res.json(data);
+};
+
+app.get("/photos", getPhotos);
+app.get("/photos/:id", getPhoto);
 
 export default app;
