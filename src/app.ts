3813b7204763064aba8624c742dcb4a41b4927fd
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-20 19:28:25 +0300

    useing redis

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,61 @@
 import express, { RequestHandler } from "express";
 import cors from "cors";
 import axios from "axios";
+import { createClient } from "redis";
+
+const redisClient = createClient();
 
 const app = express();
 
 app.use(cors());
 
+const start = async () => {
+  try {
+    await redisClient.connect();
+    console.log("Redis connected ✅");
+  } catch (err) {
+    console.error("Failed to start:", err);
+  }
+};
+
+start();
+
+const getOrSetCache = async <T>(key: string, cb: () => Promise<T>) => {
+  const data = await redisClient.get(key);
+
+  if (data != null) return JSON.parse(data);
+
+  const freshData = await cb();
+  await redisClient.setEx(key, 3600, JSON.stringify(freshData));
+  return freshData;
+};
+
 const getPhotos: RequestHandler = async (req, res, next) => {
   const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/photos",
-    { params: { albumId } }
-  );
+  const key = `photos:?alumId=${albumId}`;
+
+  const data = await getOrSetCache(key, async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos",
+      { params: { albumId } }
+    );
+
+    return data;
+  });
 
   res.json(data);
 };
 
 const getPhoto: RequestHandler = async (req, res, next) => {
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-  );
+  const key = `photos:?id=${req.params.id}`;
+
+  const data = await getOrSetCache(key, async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+
+    return data;
+  });
 
   res.json(data);
 };
